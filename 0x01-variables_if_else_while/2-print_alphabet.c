40c7f46f127b5d347a3c6b8cae9f509a0cecadab
Ahmed Mekky <27medSh3pan@gmail.com>
2023-03-17 10:31:01 -0700

    here we go

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
