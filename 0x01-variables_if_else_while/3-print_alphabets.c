d925181fa69ebe5f62aa8f27f0024feec5bb4fee
Ahmed Mekky <27medSh3pan@gmail.com>
2023-03-17 16:00:46 -0700

    c is funn

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        for (int i = 97; i < 123; i++)
+        {
+                putchar(i);
+        }
+        for (int i = 67; i < 91; i++)
+        {
+                putchar(i);
+        }
+  	putchar('\n');
+ 	return (0);
+}
