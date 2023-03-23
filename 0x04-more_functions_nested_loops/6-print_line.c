f34de47298e2b5fa9e0cf0c0e0511e2b960661fd
Ahmed Mekky <27medSh3pan@gmail.com>
2023-03-23 15:43:32 -0700

    heiiiii

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = c; i > 0; i--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	return (0);
+}
