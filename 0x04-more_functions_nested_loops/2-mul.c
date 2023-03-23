bfd90bfb4f9506c5b35503fe96233bb7e84a5579
Ahmed Mekky <27medSh3pan@gmail.com>
2023-03-23 15:15:45 -0700

    hello

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *_mul - multibate two numbers
- *@c: letter being tested
- * Return: Always 0
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
-
 int mul(int a, int b)
-
 {
 	return (a * b);
 }
