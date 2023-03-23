98e98a27a5866ba7717d6ca01c6c32b2fd257081
Ahmed Mekky <27medSh3pan@gmail.com>
2023-03-23 10:32:08 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for a uppercase character
+ *@c: letter being tested
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{	
+	if(c <= 65 && c >= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
