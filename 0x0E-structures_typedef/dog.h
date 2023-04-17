48a3c48c3e3e5fcd86d1f30cbc5265e5c8822c37
Ahmed Mekky <27medSh3pan@gmail.com>
2023-04-17 14:37:48 -0700

    First try in struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: For dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+return (0);
+#endif
