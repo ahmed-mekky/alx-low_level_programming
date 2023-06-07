7c72210f69103378de43127a9ff5952b9af3d504
Ahmed-Mekkky <27medsh3pan@gmail.com>
2023-06-07 14:11:45 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - strlen
+ * @s: pointer to string
+ * Return: number of chars.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
