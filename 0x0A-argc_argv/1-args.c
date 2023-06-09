054582ad47e3286d36805fc69b197b72e212891e
Ahmed-Mekkky <27medsh3pan@gmail.com>
2023-06-09 04:50:04 -0700

    ARGs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print number of commands
+ *
+ * @argc: int
+ * @argv: string
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	_putchar(argc);
+	_putchar('\n');
+	return (0);
+}
