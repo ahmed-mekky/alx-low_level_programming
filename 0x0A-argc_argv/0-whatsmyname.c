e44c243c09d906154b2e5d909149b07bf62d7ce1
Ahmed-Mekkky <27medsh3pan@gmail.com>
2023-06-09 04:31:33 -0700

    ARGs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,13 +9,13 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[argc - 1][i] != '\0'; i++)
+	for (i = 0; argv[0][i] != '\0'; i++)
 	{
-		_putchar(argv[argc - 1][i]);
+		_putchar(argv[0][i]);
 	}
 	_putchar('\n');
 	return (0);
