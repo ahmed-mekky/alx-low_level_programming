5c692e4cfda9fb927997887f7353f8da03802323
Ahmed-Mekkky <27medsh3pan@gmail.com>
2023-06-09 09:05:06 -0700

    ARGs

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - calc the change
+ * @argc: int
+ * @argv: string
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int change, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n ");
+		return (1);
+	}
+	change = atoi(argv[1]);
+	if (change <= 0)
+	{
+		printf("0\n");
+	}
+	while (change > 0)
+	{
+		coins++;
+		if (change >= 25)
+		{
+			change -= 25;
+		}
+		else if (change >= 10)
+		{
+			change -= 10;
+		}
+		else if (change >= 5)
+		{
+			change -= 5;
+		}
+		else if (change >= 2)
+		{
+			change -= 2;
+		}
+		else if (change >= 1)
+		{
+			change -= 1;
+		}
+	}
+	return (0);
+}
