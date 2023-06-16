dc941f61f93c0980a855c21877c57eb767fc2b1c
root <root@DESKTOP-7HAGQJD.localdomain>
2023-06-16 05:35:20 +0300

    more Malloc :)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - check the code
+ * @b: unsigned int
+ * Return: void.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+}
