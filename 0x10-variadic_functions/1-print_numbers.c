974b60140c7e67e751e47768f4eccda7790b5b9a
ahmed-mekky <27medsh3pan@gmail.com>
2023-06-23 01:28:15 +0300

    Variadic Functions :)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,8 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-			separator = "\n";
+			separator = "";
 		printf("%d%s", va_arg(args, int), separator);
 	}
 	va_end(args);
+	putchar('\n');
 }
