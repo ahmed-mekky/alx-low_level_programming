ad6bd71e38bb0681690cdb50527be24f15436a0d
ahmed-mekky <27medsh3pan@gmail.com>
2023-06-23 02:39:44 +0300

    Variadic Functions :)

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print unknown number of anything
+ * @format: ...
+ * Return: Nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0, n = 0;
+	char *separator, *str;
+
+	separator = ", ";
+	while (format[n])
+		n++;
+	va_start(args, format);
+	while (i < n)
+	{
+		if (i == n - 1)
+			separator = "\n";
+		switch (format[i])
+		{
+			case 'i':
+				printf("%i%s", va_arg(args, int), separator);
+				break;
+			case 'c':
+				printf("%c%s", va_arg(args, int), separator);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(args, double), separator);
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				printf("%s%s", str ? str : "(nil)", separator);
+				break;
+		}
+		i++;
+	}
+	va_end(args);
+}
