7eabe266c68bcf0fd0dc01ce51a05419ae9916b1
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-02 08:08:37 +0300

    Singly Linked Lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,19 @@
 #include "lists.h"
 
+/**
+ * free_list - add new node at th end
+ *
+ * @head: node to free its list
+ *
+ * Return: Nothing
+ */
+
 void free_list(list_t *head)
 {
-	while(head)
+	while (head)
 	{
 		list_t *tofree = head;
+
 		head = head->next;
 		free(tofree->str);
 		free(tofree);
