132dc0e708fc99a69e0bd1fa49295da56e9385e6
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-02 06:02:52 +0300

    Singly Linked Lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - print a list of nodes
+ *
+ * @h: queue of nodes
+ *
+ * Return: number of nodes printed.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
