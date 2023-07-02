c0ef75710f54ce94fc1795157bb48a9dba1e798e
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-02 07:37:56 +0300

    Singly Linked Lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_node_end - add new node at th end
+ *
+ * @head: exit node
+ * @str: string to add in node
+ *
+ * Return: address of added node.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode = malloc(sizeof(list_t));
+	list_t *idk;
+
+	if (!str || !newNode)
+	{
+		return (NULL);
+	}
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+
+	if (!newNode->str)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = NULL;
+
+	if(!*head)
+	{
+		*head = newNode;
+	}
+	else
+	{
+		idk = *head;
+		idk->next = newNode;
+		*head = idk;
+	}
+	return (newNode);
+}
