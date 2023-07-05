af4ebe01b388e67c5c30a8c39b92f8dd66c3bdf4
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-05 04:12:30 +0300

    More Singly Linked Lists :(

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
-	for (i = 0; i < index; i++)
+	while (head)
 	{
-		head = head->next;
+		for (i = 0; i < index; i++)
+		{
+			head = head->next;
+		}
+		return (head);
 	}
-	return (head);
+	return (NULL);
 }
