54f971c9d07f62477994361fc3fd5a8cd5d5e6c7
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-05 03:24:15 +0300

    More Singly Linked Lists :(

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - add node
+ *@head: list of nodes
+ * Return: address of the last node.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *idk;
+
+	while (*head)
+	{
+		idk = *head;
+		*head = (*head)->next;
+		free(idk);
+	}
+}
