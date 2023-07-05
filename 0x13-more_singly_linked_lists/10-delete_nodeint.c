326c6aa8ccc899e797a4f64da82fabdf31fff270
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-05 06:00:06 +0300

    More Singly Linked Lists :(

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		for (i = 0; i < index - 1; i++)
 		{
 			if (!copy)
-				return (NULL);
+				return (-1);
 			copy = copy->next;
 		}
 	next = copy->next;
@@ -23,6 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		copy = *head;
 		*head = copy->next;
+		free(copy);
 		return (1);
 	}
 }
