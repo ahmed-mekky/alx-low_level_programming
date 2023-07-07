a06652b72f212e9523b29c8ca061ce1b25a15a8d
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-07 18:32:14 +0300

    Bit Manipulation :(

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,7 +16,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	n = (n >> (index));
 	if (n & 1)
 		return (1);
-	else if (n == 0)
+	else if (n == 0 || index > 32)
 		return (-1);
 	else
 		return (0);
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -10,11 +10,11 @@ int main(void)
 {
     int n;
 
-    n = get_bit(0, 0);
+    n = get_bit(1, 0);
     printf("%d\n", n);
-    n = get_bit(98, 1);
+    n = get_bit(98, -3);
     printf("%d\n", n);
-    n = get_bit(1024, 0);
+    n = get_bit(-1024, 15);
     printf("%d\n", n);
     return (0);
 }
