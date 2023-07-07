633316fc1cb528082d0b2d8f21d17e78cc182209
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-07 11:11:12 +0300

    Bit Manipulation :(

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,23 +15,23 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
-	for (i = 61; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
 		idk = (1 << i);
 		if (n & idk)
 		{
-			_putchar('1');
+			putchar('1');
 			i--;
 			while (i >= 0)
 			{
 				idk = (1 << i);
 				if (n & idk)
-					_putchar('1');
+					putchar('1');
 				else
-					_putchar('0');
+					putchar('0');
 				i--;
 			}
 		return;
