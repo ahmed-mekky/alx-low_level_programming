389adae1aeddd99b3c2c2560c557ab40323c9dc7
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-07 05:32:46 +0300

    Bit Manipulation :(

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * binary_to_uint - convert binary to dec
  *
@@ -6,29 +7,48 @@
  *
  * Return: result or 0.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i, n, result = 0;
+	unsigned int i, n = 0, result = 0;
 
 	if (!b)
 	{
 		return (0);
 	}
-	n = strlen(b);
-	for (i = 0; b[i]; i++)
+	while (b[n])
+		n++;
+
+	for (i = 0; i < n; i++)
 	{
-		if (b[i] == '1')
+		if (b[i] == '1' || b[i] == '0')
 		{
-			result += pow(2, n - i);
-		}
-		else if (b[i] == '0')
-		{
-			result += 0;
+			result += power(2, n - i - 1) * (b[i] - '0');
 		}
 		else
 		{
 			return (0);
 		}
+	}
+	return (result);
+}
+/**
+ * power - power
+ *
+ *@x: ...
+ *@y: ....
+ *
+ * Return: ......
+ */
+unsigned int power(unsigned int x, unsigned int y)
+{
+	unsigned int i, result = 1;
+
+	if (y == 0 || x == 0)
+		return (1);
+	for (i = 0; i < y; i++)
+	{
+		result *= x;
 	}
 	return (result);
 }
