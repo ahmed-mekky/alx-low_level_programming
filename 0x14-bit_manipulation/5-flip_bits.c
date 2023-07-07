82cbe43a6b065824da8f0241debc59bb5994ae16
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-07 19:48:43 +0300

    Bit Manipulation :(

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - ....
+ *
+ * @n: .......
+ * @m: .....
+ *
+ * Return: ......
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int idk, idk2;
+	int i, idk3 = 0;
+
+	idk = n ^ m;
+	for (i = 31; i >= 0; i--)
+	{
+		idk2 = idk >> i;
+		if (1 & idk2)
+			idk3++;
+	}
+	return idk3;
+}
