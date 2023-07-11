a2e671f1a1a73f5a4a852c5d0b899a1a9a338b8c
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 05:38:17 +0300

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * read_textfile - copy & paste
+ *
+ * @filename: ....
+ * @letters: .....
+ *
+ * Return: ..
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+}
