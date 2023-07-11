db5e1acf48e62f2a07cf2098a991808030bb6f5a
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 15:26:04 +0300

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,40 +12,23 @@
 
 int main(int argc, char **argv)
 {
-	int fd, fd2, x, y;
-	ssize_t n_rd, n_wr;
-	char buffer[BUF_SIZE];
-	char *filename = argv[1], *filename2 = argv[2];
+	int fd, fd2, x, y, n_rd, n_wr;
+	char *filename = argv[1], *filename2 = argv[2], buffer[BUF_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (!filename)
+	fd = open(filename, O_RDONLY), fd2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd < 0 || fd2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		if (fd < 0)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		if (fd2 < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
 		exit(98);
 	}
-	if (!filename2)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename2);
-		exit(98);
-	}
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	fd2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC);
-	if (fd2 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
-		exit(99);
-	}
-
 	n_rd = read(fd, buffer, BUF_SIZE);
 	if (n_rd < 0)
 	{
@@ -53,27 +36,21 @@ int main(int argc, char **argv)
 		close(fd);
 		exit(98);
 	}
-
 	n_wr = write(fd2, buffer, n_rd);
-	if (n_wr != n_rd)
+	if (n_wr < 0 || n_wr != n_rd)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd2);
 		close(fd2);
 		exit(98);
 	}
-
-	x = close(fd);
-	if (x < 0)
+	x = close(fd), y = close(fd2);
+	if (x < 0 || y < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		if (x < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		if (y < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
-	y = close(fd2);
-	if (y < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
-
 	return (0);
 }
