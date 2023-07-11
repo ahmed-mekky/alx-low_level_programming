92ee64c76619e078b1a794cd796a3cf597c9e9d7
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 17:55:34 +0300

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,32 +13,32 @@
 int main(int argc, char **argv)
 {
 	int fd, fd2, x, y, n_rd;
-	char *filename = argv[1], *filename2 = argv[2], buffer[BUF_SIZE];
+	char buffer[BUF_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(filename, O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((n_rd = read(fd, buffer, BUF_SIZE)) > 0)
 	{
 		if (fd2 < 0 || write(fd2, buffer, n_rd) != n_rd)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd);
 			exit(99);
 		}
 	}
 	if (n_rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	x = close(fd), y = close(fd2);
