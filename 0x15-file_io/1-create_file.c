88b15c4f7f61bbb86c2750ac02f8413b9a3a62ff
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 10:14:22 +0300

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
-	to_wr = strlen(text_content) + 1;
+	to_wr = strlen(text_content);
 	n_wr = write(fd, text_content, to_wr);
 	if (n_wr == -1 || n_wr != to_wr)
 	{
