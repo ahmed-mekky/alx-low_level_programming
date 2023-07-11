b31aa50fd0e76032eed5112b28eced177d3564bb
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 10:19:37 +0300

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-		return (1);
-		
+		return;
+
 	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
 	{
