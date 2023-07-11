ae1b43b5df4bf37bb02d4b993d834ed4878badca
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-11 17:43:46 +0300

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv)
 {
-	int fd, fd2, x, y, n_rd, n_wr;
+	int fd, fd2, x, y, n_rd;
 	char *filename = argv[1], *filename2 = argv[2], buffer[BUF_SIZE];
 
 	if (argc != 3)
@@ -27,18 +27,19 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	fd2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	n_rd = read(fd, buffer, BUF_SIZE);
+	while ((n_rd = read(fd, buffer, BUF_SIZE)) > 0)
+	{
+		if (write(fd2, buffer, BUF_SIZE) != n_rd)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
+			close(fd2);
+			exit(98);
+		}
+	}
 	if (n_rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd);
 		close(fd);
-		exit(98);
-	}
-	n_wr = write(fd2, buffer, n_rd);
-	if (n_wr < 0 || n_wr != n_rd)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd2);
-		close(fd2);
 		exit(98);
 	}
 	x = close(fd), y = close(fd2);
