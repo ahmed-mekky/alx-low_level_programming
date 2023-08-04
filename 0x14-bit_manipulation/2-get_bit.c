f14849e0d769fc3dcb605281db0fac5166f056f5
ahmed-mekky <27medsh3pan@gmail.com>
2023-08-04 05:48:45 +0300

    Bit Manipulation :)

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,16 @@
+
+/**
+ * get_bit - get a bit
+ *
+ * @n: ...
+ * @index: ....
+ *
+ * Return: 1 if no errors, -1 if error.
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 32)
+		return (-1);
 	return (n >> index) & 1;
 }
