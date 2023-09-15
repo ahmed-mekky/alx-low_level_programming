9ea05b4658d51499effd780e6dd0fa2b0ae6b2fc
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 05:01:04 +0300

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,8 @@
 /**
  * dlistint_len - get len of a doubly linked list
  *
- * @h: queue of nodes
+ * @head: queue of nodes
+ * @n: value of the new head node
  *
  * Return: number of nodes printed.
  */
@@ -12,9 +13,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 	(*head)->prev = new;
-	return (new)
+
+	return (new);
 }
