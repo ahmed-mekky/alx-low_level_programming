7773fd5a44a5e5e5c9357242fdecb3f6a95947c4
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 08:13:36 +0300

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node = head;
 	unsigned int i = 0;
 
-	if (!node)
-		return (NULL);
 	while (node->next && i < index)
 	{
 		node = node->next;
