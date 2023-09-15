c81a31204514fe302ed7dad0e403e3ce9436b2f6
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 08:28:10 +0300

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *old = *h;
-	unsigned int 1;
+	unsigned int i = 1;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
