fdfb9d1840c2cc93ff7f5d7efcb9ac5e627dc49f
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 04:41:27 +0300

    Doubly Linked Lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - print a list of nodes
+ *
+ * @h: queue of nodes
+ *
+ * Return: number of nodes printed.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
