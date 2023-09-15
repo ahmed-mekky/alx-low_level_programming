118a409fc165fc29fb970520416feaa48fc9f614
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 05:57:03 +0300

    Doubly Linked Lists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum a list
+ *
+ * @head: queue of nodes
+ *
+ * Return: the sum.
+ */
+
+int sum_dlistint(dlistint_t *head)
+	int sum = 0;
+
+	while (head)
+	{
+		head = head->next;
+		sum += head->n;
+	}
+	return (sum);
+}
