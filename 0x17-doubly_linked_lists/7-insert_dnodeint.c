51ced21044669e9a0a21e390212eeee7f6cf164f
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-16 05:12:56 +0300

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,8 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = *h;
 		new->prev = NULL;
-		if (*h)
-			(*h)->prev = new;
+		if (old)
+			old->prev = new;
 		*h = new;
 		return (new);
 	}
