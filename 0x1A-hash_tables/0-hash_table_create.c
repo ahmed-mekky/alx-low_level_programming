a19766640931602a8d77e7423d58b4e791d68322
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 00:56:14 +0200

    Hash Tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tbl = malloc(sizeof(hash_table_t) * size);
+	unsigned long int i;
+
+	tbl = malloc(sizeof(hash_table_t));
+	if (tbl == NULL)
+		return (NULL);
+
+	tbl->size = size;
+	tbl->array = malloc(sizeof(hash_node_t *) * size);
+	if (tbl->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		tbl->array[i] = NULL;
+
+	return (tbl);
+}
