f99cefe1ea99df48e3efc55cbd87ccd7c1f1660c
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 01:13:14 +0200

    Hash Tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - find index of a key
+ *
+ * @key: key passed
+ * @size: size of the hash tables
+ *
+ * Return: the key's index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+		return (0);
+
+	return (hash_djb2(key) % size);
+}
