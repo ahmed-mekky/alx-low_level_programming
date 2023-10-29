276fc767bed337767768a6b9a9f0b04b0372d358
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 00:58:52 +0200

    Hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,6 +2,10 @@
 
 /**
  * hash_table_create - create a hash table
+ *
+ * @size: size of the array of the hash table
+ *
+ * Return: hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
