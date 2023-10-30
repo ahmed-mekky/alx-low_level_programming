9545de94b21e38bc3b95dff4ee3a333ab8bf0404
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 02:14:30 +0200

    Hash Tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,6 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL)
 		return (0);
 
+	index = key_index((unsigned char *)key, ht->size);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -35,11 +37,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array == NULL)
 		return (0);
-	new_node->next = NULL;
-	ht->array[index] = new_node;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+	if (strcmp(key, ht->array[index]->key) == 0)
+	{
+		ht->array[index]->value = strdup(value);
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return(1);
+	}
+	
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
 	return (1);
 }
