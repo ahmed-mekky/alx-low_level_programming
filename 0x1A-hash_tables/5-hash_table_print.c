3bb6878c138278ff9536840823870451980a393b
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 02:27:21 +0200

    Hash Tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints key and value of a hash table in order
+ *
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *node;
+	char *del = "";
+
+	if (!ht || !ht->array)
+		return;
+
+	putchar('{');
+	while (idx < ht->size)
+	{
+		node = (ht->array[idx]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", del, node->key, node->value);
+			del = ", ";
+			node = node->next;
+		}
+		idx++;
+	}
+	putchar('}');
+}
