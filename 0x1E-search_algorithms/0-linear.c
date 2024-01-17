74c8b728bc84b589183e6e243cbb2c0b56945385
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-18 01:09:58 +0200

    Search Algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search function :)
+ *
+ * @array: pointer
+ * @size: int
+ * @value: int
+ *
+ * return the index of the value
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, *array);
+		if (*array == value)
+			return (i);
+		else
+			array++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGOS_1
+#define SEARCH_ALGOS_1
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
